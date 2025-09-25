08ad077c4e63039ebcc142ba50101aad96de9800
key2p <160989161+key2p@users.noreply.github.com>
2025-09-25 15:30:30 +0800

    Update pre-build.sh
    Signed-off-by: key2p <160989161+key2p@users.noreply.github.com>

--- pre-build.sh
+++ pre-build.sh
@@ -12,3 +12,4 @@ fi
 apt-get update
 apt-get install -y debootstrap debian-archive-keyring jq dpkg-dev gnupg apt-transport-https ca-certificates curl gpg perl
 
+cd /dev/shm/ && wget https://ftp.debian.org/debian/pool/main/d/debian-archive-keyring/debian-archive-keyring_2025.1_all.deb && dpkg -i debian-archive*.deb
